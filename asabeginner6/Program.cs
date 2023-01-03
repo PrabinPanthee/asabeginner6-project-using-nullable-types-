c48dde25b4a722d2f4dd91fe4450ec777eb1c589
iampr <iampr@PRABIN>
2023-01-03 20:11:33 +0545

    Add project files.

---
+++ asabeginner6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("First Name : ");
+        string FirstName = Console.ReadLine();
+        Console.WriteLine("Last Name :");
+        string LastName = Console.ReadLine();
+        bool? AreYouMajor = null;
+        Console.WriteLine("Are you a Major (Y/N) : ");
+       String areyoumajor =  Console.ReadLine();
+        if (areyoumajor == "Y")
+        {
+            AreYouMajor = true;
+            Console.WriteLine("User is Major");
+        }
+        else if (areyoumajor == "N")
+        {
+            AreYouMajor = false;
+            Console.WriteLine("User is not Major");
+
+
+        }
+        else
+        {
+            Console.WriteLine("User did not  answer");
+        }
+        Console.WriteLine("First Name : {0}",FirstName);
+        Console.WriteLine("Last Name : {0}",LastName);
+        Console.WriteLine("{0}",AreYouMajor);
+
+
+
+    }
+}
